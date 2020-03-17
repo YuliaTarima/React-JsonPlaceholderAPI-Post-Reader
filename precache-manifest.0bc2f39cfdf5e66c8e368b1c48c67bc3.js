92819c0dff9ac7c28460426050e6fc69020e3a65
Yulia Tarima <yulia.tarima@gmail.com>
2020-03-17 16:30:08 -0700

    Updates

---
+++ precache-manifest.0bc2f39cfdf5e66c8e368b1c48c67bc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d61d69c54b8a67105e1c5fd891822c4",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/index.html"
+  },
+  {
+    "revision": "19b8b72bec9f379cdf59",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "0a7b12cef37e0b471d34",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/css/main.db75f167.chunk.css"
+  },
+  {
+    "revision": "19b8b72bec9f379cdf59",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/js/2.e97a772a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/js/2.e97a772a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7b12cef37e0b471d34",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/js/main.1723e33c.chunk.js"
+  },
+  {
+    "revision": "d3019e0f39df7042c411",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/js/runtime-main.0b951732.js"
+  },
+  {
+    "revision": "56688d3d9138d38a7fc399294efa6060",
+    "url": "/React-JsonPlaceholderAPI-Post-Reader/static/media/logo.56688d3d.jpg"
+  }
+]);
